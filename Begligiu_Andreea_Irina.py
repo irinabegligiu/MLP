ed919e4e4f577b530b7f95ac43977c1276fb268f
Irina <106312443+irinabegligiu@users.noreply.github.com>
2022-05-26 13:58:00 +0300

    Update Begligiu_Andreea_Irina.py

--- Begligiu_Andreea_Irina.py
+++ Begligiu_Andreea_Irina.py
@@ -1,47 +1,47 @@
 import numpy as np
 from sklearn import neural_network
 
-#citim si importam fisierul cu baza de date intr-o matrice numpy
+#read and import the database file into a numpy matrix
 data=np.genfromtxt(r'data.txt')
-#baza de date contine 4 coloane: primele 3 de date si ultima de etichete
-#baza de date are doua etichete(categorii) distincte
+#the database contains 4 columns: data on the first 3 and the tag on the last
+#the database has two separate tags
 
-X=data[:,:3] #Includem in matricea X primele 3 coloane
-Y=data[:,3] #Includem coloana 4 intr-un sir de etichete Y
+X=data[:,:3] #first 3 collumns in the X matrix
+Y=data[:,3] #fourth column in a tag array "Y"
 
-#initializam matricile X de training + test ==================================
+#initializing the training matrices
 X_train=np.concatenate((X[:int(0.75*50859)], X[50859:int(0.75*194198)]))
 Y_train=np.concatenate((Y[:int(0.75*50859)], Y[50859:int(0.75*194198)]))
-#X_train, Y_train = 75% din (prima categorie; 50859 de elemente) + 
-# + 75% din (a doua categorie; 194198 de elemente)
+#X_train, Y_train = 75% of (first category; 50859 elements) + 
+# + 75% of (second category; 194198 elements)
 
-#initializam sirurile Y de training + test ===================================
+#initializing the testing matrices
 X_test=np.concatenate((X[int(0.75*50859):50859], X[int(0.75*194198):194198]))
 Y_test=np.concatenate((Y[int(0.75*50859):50859], Y[int(0.75*194198):194198]))
-#X_test, Y_test = 25% din (prima categorie; 50859 de elemente) +
-# + 25% din (a doua categorie; 194198 de elemente)
+#X_test, Y_test = 25% of (first category; 50859 elements) +
+# + 25% of (second category; 194198 elements)
 
-def rata(pred, test): #functie pentru a calcula rata de succes a clasificarii
+def rata(pred, test): #success rate function
     nr=0
-    for i in range (len(X_test)):  #pentru fiecare valoare de test
-        if pred[i]==test[i]: #verificam daca predictia a fost corecta
-            nr+=1 #numaram de cate ori a fost corecta
-    return 100*nr/len(X_test) #calculam procentajul (rata de succes)
+    for i in range (len(X_test)):  #for every value
+        if pred[i]==test[i]: #we check if the prediction was correct
+            nr+=1 #it counts the correct predictions
+    return 100*nr/len(X_test) #return the percentage
 
 
-#calculam media acuratetei pentru antrenari si testari in 15 iteratii
+#we calculate the average accuracy for training and testing in 15 iterations
 suma=0
 for i in range(15):
     clf=neural_network.MLPClassifier(hidden_layer_sizes=(10,10), learning_rate_init=0.1, max_iter=1500)
     
-    #antrenam algoritmul  
+    #algorithm train
     clf.fit(X_train,Y_train)
     
-    #testam algoritmul
+    #algorithm test
     predictie=clf.predict(X_test)
     
-    #calculam rata de succes a clasificarii folosind functia <<rate(pred, test)>> definita anterior
+    #success rate calculator
     suma = suma + rata(predictie, Y_test)
-print(str(round(suma/15, 4)) + "%") #afisam procentajul acuratetei rotunjit la 4 zecimale
+print(str(round(suma/15, 4)) + "%") #return the accuracy percentage with a precision of 4 decimals
 
 
